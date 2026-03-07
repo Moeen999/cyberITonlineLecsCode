1bbb6709af25022793d8acaad08a10b9a1308f88
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-07 21:50:04 +0500

    JS started intro lectures about js core concepts

---
+++ JS LEC 1/script.js
@@ -0,0 +1,18 @@
+// let a = 12;
+// a = 14;
+// console.log(a);
+
+// const a = 12;
+// a=14;
+// console.log(a)
+
+// var a = 12;
+// a=14;
+// console.log(a)
+
+
+// let a; //! Decalration
+// a = 12; //! Initialization
+
+// let name = 1;
+// console.log(typeof(name));
