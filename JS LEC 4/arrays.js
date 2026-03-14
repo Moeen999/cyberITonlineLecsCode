7a3c4767aefa5a2b1e3807f48fdf50e15fa6a33e
Muhammad Moeen <mooenmalik374@gmail.com>
2026-03-14 20:35:34 +0500

    JS LEC 4 Day 4 files code

---
+++ JS LEC 4/arrays.js
@@ -0,0 +1,56 @@
+const obj = {
+  name: "Moeen Malik",
+  gender: "Male",
+  isStudent: true,
+};
+
+// const cars = [];
+// cars[0]= "honda civic";
+// cars[1]= "toyota";
+// cars[2]= "lexus l7";
+
+// const cars = new Array("Saab", "Volvo", "BMW");
+// const fruits = ["Banana", "Orange", "Apple", "Mango"].toString();
+
+// console.log(fruits)
+// console.log(cars);
+
+// console.log(typeof(arr));
+
+// const myArr = [12,13,obj];
+// console.log(myArr[2].name)
+
+// const fruitsContainer = document.getElementById("fruits-container");
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let fLen = fruits.length;
+
+// let text = "<ul>";
+// for (let i = 0; i < fLen; i++) {
+//   text += "<li>" + fruits[i] + "</li>";
+// }
+
+// fruits.forEach((fruit) => {
+//   text += `<li>  ${fruit}  </li>`;
+// });
+// text += "</ul>";
+// fruitsContainer.innerHTML = text;
+
+
+// const fruits = ["Banana", "Orange", "Apple"];
+// fruits[3] = "Lemon"; 
+// fruits.push("Lemon")
+// console.log(fruits)
+
+const fruits = ["Banana", "Orange", "Apple"];
+const isArr = Array.isArray(fruits);
+console.log(isArr)
+// fruits[6] = "Lemon";
+// console.log(fruits)
+
+// const nameObj={
+//     name:"Moeen Malik"
+// }
+
+// const name = nameObj["name"];
+// console.log(name)
